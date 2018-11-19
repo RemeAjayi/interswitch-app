1935b6f54d6d32737609714cd98d614a1befd808
Olohireme Ajayi <remeajayi@gmail.com>
2018-11-19 17:54:25 +0100

    dropdown in progress

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -8,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class DropdownComponent implements OnInit {
 
   constructor() { }
+  
+  showMenu(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.toggle-icon')) {
+
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 
   ngOnInit() {
   }
